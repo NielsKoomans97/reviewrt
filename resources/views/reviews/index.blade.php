f4741b7b39b7a8366e206dfa37c0f5fb16c69828
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2024-09-20 02:01:36 +0200

    nog een hoop teringzooi

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="review-list">
         @foreach ($reviews as $review)
-            <a class="review">
+            <a class="review" href="{{ route('reviews.show', $review) }}">
                 <h3 class="title">{{ $review->title }}</h3>
                 <span class="comment_count"><b>{{ count($review->comments) }}</b> opmerkingen</span>
                 <span class="creator">Gemaakt door <b>{{ $review->creator->name }}</b></span>
--- resources/views/reviews/view.blade.php
+++ resources/views/reviews/view.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+@extends('templates.page')
+
+@section('content')
+<div class="review-details">
+    <h2>{{ $review->title }}</h2>
+    <span class="review-user">door <b>{{ $review->creator->name }}</b></span>
+    <span class="review-summary">{{ $review->description }}</h2>
 </div>
+<div class="comments">
+    <div class="new-comment">
+    </div>
+</div>
+@endsection

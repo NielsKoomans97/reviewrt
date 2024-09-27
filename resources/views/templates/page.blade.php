29525c7ab3ca549731cb7b8d1873ab1ed8764d49
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2024-09-27 06:22:38 +0200

    nieuwe sass

--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <script src="/js/controls.js" type="module" defer></script>
+    <link rel="stylesheet" href="build/assets/main.css">
 </head>
 
 <body>
